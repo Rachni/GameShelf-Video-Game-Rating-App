ad22d4ddc79e1ac9c41905286f5f0cda9a9aba85
Ana <anamartinezperdomo@gmail.com>
2025-05-24 12:59:59 +0100

    first front deployment try

--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,38 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
-            refresh: true,
-        }),
-        react({
-            jsxRuntime: "automatic",
-            fastRefresh: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.jsx"],
+      refresh: true,
+    }),
+    react({
+      jsxRuntime: "automatic",
+      fastRefresh: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/resources/js",
     },
-    optimizeDeps: {
-        include: ["react", "react-dom", "react-router-dom"],
-    },
-    esbuild: {
-        loader: "jsx",
-        include: /\.[jt]sx?$/,
-        exclude: [],
-    },
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://localhost:8000", // URL de tu backend de Laravel
-                changeOrigin: true, // Cambia el origen de la solicitud al backend
-                secure: false, // Desactiva la verificación de certificados SSL (útil en desarrollo)
-            },
-        },
-    },
+  },
+  build: {
+    outDir: "public/build", 
+    emptyOutDir: true,
+    manifest: true, 
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: process.env.NODE_ENV === 'development' ? {
+      host: 'localhost',
+      protocol: 'ws'
+    } : false,
+    cors: true
+  }
 });
